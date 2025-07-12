7c614e63ae38434cace2f73a6bf55b3dc9d07511
BlitzNoval <ljmoodley03@gmail.com>
2025-07-12 10:51:55 +0200

    MainMenu

---
+++ Assets/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("UI References")]
+    public GameObject controlsPanel;
+
+    // Called when "Play" is pressed
+    public void PlayGame()
+    {
+        // Load the next scene in build order
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    // Called when "Controls" is pressed
+    public void ShowControls()
+    {
+        controlsPanel.SetActive(true);
+    }
+
+    // Called when "Close" is pressed in controls panel
+    public void CloseControls()
+    {
+        controlsPanel.SetActive(false);
+    }
+
+    // Called when "Quit" is pressed
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game...");
+        Application.Quit();
+    }
+}
